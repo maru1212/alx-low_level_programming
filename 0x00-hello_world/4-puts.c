01386d3e21dedc3aebca9dfdbc55115c2877b348
Maranata <maranataflk9@gmail.com>
2022-04-07 09:52:45 -0700

    using put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* #include <string.h> */
 int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
