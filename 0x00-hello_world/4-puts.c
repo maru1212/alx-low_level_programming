f29c94710ee368eaa370487b637101d0dfc0eaea
Maranata <maranataflk9@gmail.com>
2022-04-07 09:42:09 -0700

    pringing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* #include <string.h> */
 int main()
 {
-	char str[65] = "Programming is like building a multilingual puzzle\n";
+	char str[] = "Programming is like building a multilingual puzzle\n";
 	puts(str);
 	return (0);
 }
