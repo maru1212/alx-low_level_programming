2d40d0a9b1b2dc5b595035a07db46393305846d3
Maranata <maranataflk9@gmail.com>
2022-04-07 10:29:33 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* #include <string.h> */
-int main(void)
+int main(void)  /* prints the word in the function */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
