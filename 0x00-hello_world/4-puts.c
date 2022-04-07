ed593a719d465f789ee7bb296ea4623b0e2e87ca
Maranata <maranataflk9@gmail.com>
2022-04-07 06:48:39 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+/* #include <string.h> */
 int main()
 {
-	char str[65];
-	strcpy(str, "Programming is like building a multilingual puzzle\n");
+	char str[65] = "Programming is like building a multilingual puzzle\n";
 	puts(str);
 	return (0);
 }
