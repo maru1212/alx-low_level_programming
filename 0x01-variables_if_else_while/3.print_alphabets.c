deda5623160f1e9be6cf1dfaf065dbb9d57ffb82
Maranata <maranataflk9@gmail.com>
2022-04-08 10:10:36 -0700

    small and capital alphabet

---
+++ 0x01-variables_if_else_while/3.print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char small = 'a';
+	char capital = 'A';
+	char newline = '\n';
+
+	for (small = 'a'; small <= 'z'; small++)
+		putchar(small);
+	for (capital = 'A'; capital <='Z'; capital++)
+		putchar(capital);
+	putchar(newline);
+	return (0);
+}
+
