51518d7345890a2f4ae8b23d8706ba064b9d1c9e
Maranata <maranataflk9@gmail.com>
2022-04-09 13:00:51 -0700

    i did itgit add .

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	int c = 0;
+
+	for (n = '0'; n <= '8'; n++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			
+			if (n < c)
+			{
+				
+				putchar(n);
+				putchar(c);
+				if ((n != '8') && (c != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
