18bcaa6581cf348d2d83704b62ce9699bbf19087
Maranata <maranataflk9@gmail.com>
2022-04-12 11:40:00 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign: prints sign of the value.
+ * @n: the value to be checked.
+ *
+ * Retrun: 1 if n is +, 0 if n is zero and -1 if n is lessthan zero.
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+	return (-1);
+}
