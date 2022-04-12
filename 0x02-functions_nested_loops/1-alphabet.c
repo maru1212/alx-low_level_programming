1eb5310f467aad29c3a40b9880ffd6c943b59bde
Maranata <maranataflk9@gmail.com>
2022-04-12 10:03:18 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphapet(void)
+{
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+	return (0);
+}
