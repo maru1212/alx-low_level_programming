3f782d8b887ffa113aaa90aef5ef12ba4bb4e3d9
Maranata <maranataflk9@gmail.com>
2022-04-12 10:21:42 -0700

    alphcabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - will print the alphabet 10x
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char letter;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,7 @@
 
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_10x(void);
+
 
 #endif /* _MAIN_H_ */
