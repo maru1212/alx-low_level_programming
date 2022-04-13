ea2d5519b8fa286e8e5b7cbafbaad40fe5ab9d15
Maranata <maranataflk9@gmail.com>
2022-04-13 11:33:57 -0700

    multi tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,8 +29,10 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 				if (c == n)
+				{
 					_putchar(' ');
 					_putchar(p + '0');
+				}
 				if ( c > n)	
 				{
 					_putchar((n / 10) + '0');
