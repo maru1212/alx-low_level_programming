6b47660057a37f8fecd3a7b2b2bb0dfbc2b3e04d
Maranata <maranataflk9@gmail.com>
2022-04-13 02:16:42 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,11 @@ void times_table(void)
 {
 	int r, c;
 
-	for (r = 0; r <= 9; r++)
+	for (r = '0'; r <= '9'; r++)
 	{
-		for (c = 0; c <= 9; c++)
+		for (c = '0'; c <= '9'; c++)
 		{
-			_putchar(c * r);
+			_putchar((c * r) + '0');
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
