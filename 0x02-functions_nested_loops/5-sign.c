2d3abe084a3a9bace5b5c93833a1df2dce2299af
Maranata <maranataflk9@gmail.com>
2022-04-13 00:51:18 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,7 @@
 /**
  * print_sign - prints sign of a number.
  * @n: the value to be checked.
- * Retrun: print_sign will be 1 if n is +,
- * 0 if n is zero,
- * -1 if n is lessthan zero.
+ * Retrun: print_sign will be 1 if n is +, 0, if n = 0 and -1 otherwise.
  */
 
 int print_sign(int n)
