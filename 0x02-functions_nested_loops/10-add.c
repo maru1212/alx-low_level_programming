e97730c9e874deb91192eaa2ac445c87a93b705d
Maranata <maranataflk9@gmail.com>
2022-04-13 04:49:16 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @x: one number to be added
+ * @y: another number to be added with
+ *
+ * Return: to addition of the two numbers.
+ */
+
+int add(int x, int y)
+{
+	return (x + Y);
+}
