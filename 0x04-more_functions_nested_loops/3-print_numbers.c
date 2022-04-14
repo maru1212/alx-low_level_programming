8c6dbe7e0fd562c0b0194a3fd61c5206a73fc50d
Maranata <maranataflk9@gmail.com>
2022-04-14 10:16:25 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9.
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while(n <= 9)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
