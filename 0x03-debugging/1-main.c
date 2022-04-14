9c0dcbff905716586da603927546e80056c2d867
Maranata <maranataflk9@gmail.com>
2022-04-14 01:12:57 -0700

    comminting infinte loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	/*while (i < 10)
 	{
-		putchar(i);
-	}      /* There is no incremental, i++ */
+		putchar(i); 
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
