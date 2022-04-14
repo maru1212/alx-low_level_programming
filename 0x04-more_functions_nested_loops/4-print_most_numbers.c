268065bfb5fe58f3172cde13298ea96ed85c3141
Maranata <maranataflk9@gmail.com>
2022-04-14 10:38:52 -0700

    some number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 -9 except 2 & 4.
+ *
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+		n++;
+	}
+	_putchar('\n');
+}
