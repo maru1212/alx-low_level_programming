02671200790de90e7b5cd83f00ec0a2e9d05699c
Maranata <maranataflk9@gmail.com>
2022-04-15 05:50:55 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
+ * print_triangle - prints a triangle pattern
+ * @size: is the size of the triangle.
  *
  *
- *
- *
- *
+ * Retrurn:void.
  */
 
 void print_triangle(int size)
 {
 	int r, c, space;
 
-	for (r = 1; r <= size; r--)
+	for (r = 1; r <= size; r++)
 	{
 		for (space = 0; space < size - r; space++)
 		{
