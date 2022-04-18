63b0ff5ac8db901eb909880bb136524ef1767d9a
Maranata <maranataflk9@gmail.com>
2022-04-18 13:27:47 -0700

    two steps

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - puts char within 2 steps
+ *
+ * @str: the string.
+ *
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
