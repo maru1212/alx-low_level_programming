6bcb46179846b29cf956239cf0b48091832b09bd
Maranata <maranataflk9@gmail.com>
2022-04-18 11:34:22 -0700

    sting count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the number of charactors.
+ * @s: are the characters to be counted
+ *
+ *
+ * Retrun: to count.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '/0')
+	{
+		i++;
+	}
+
+	return (i);
+}
