c58710c13231ed79907064e08ecbdab4d9c13a65
Maranata <maranataflk9@gmail.com>
2022-04-18 06:23:29 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the number to 9.
+ * @*n: a pointer which points to the provided number.
+ *
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Here are all task of 0x05-pointers_arrays_strings
