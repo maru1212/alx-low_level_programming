fa4c67c80307106f6cd93a82f19db4d584b8e370
Maranata <maranataflk9@gmail.com>
2022-04-24 11:04:09 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,12 @@
  * _strncat - appends strings
  * @dest: the destination of the string to be appended
  * @src: the characters to be appended
+ * @n: the digit
  *
  * Return: dest
  */
 
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
@@ -18,11 +19,15 @@ char *_strncat(char *dest, char *src)
 
 	j = 0;
 
-	while (src[j] != '\0')
+	while (src[j] != '\0' && n > j)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
+	}
+	if (n > 0)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
