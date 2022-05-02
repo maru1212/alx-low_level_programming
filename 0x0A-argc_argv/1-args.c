5c86c7c98d403ca598e52736db7d6b9e9e97cdff
Maranata <maranataflk9@gmail.com>
2022-05-02 07:40:04 -0700

    task2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv: list
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
