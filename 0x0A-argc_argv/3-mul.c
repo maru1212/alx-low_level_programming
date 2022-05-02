f1914e4a401e07fa95861112773fa9553be3866a
Maranata <maranataflk9@gmail.com>
2022-05-02 08:12:23 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the product of the arguments
+ * @argc: int
+ * @argv: list
+ *
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int c = atoi(argv[1]) * atoi(argv[2]);
+
+	if (argc == 3)
+	{
+		printf("%d\n", c);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
