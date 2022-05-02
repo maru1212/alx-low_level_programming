591c759b618c894325019ec57018eb482ad6c917
Maranata <maranataflk9@gmail.com>
2022-05-02 08:20:44 -0700

    taks

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - print the product of the arguments
+ * main - print the result of the multiplication, followed by a new line
  * @argc: int
  * @argv: list
- *
- *
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	int c = atoi(argv[1]) * atoi(argv[2]);
+(void)argc;
 
-        (void)argc;
-	if (argc == 3)
-	{
-		printf("%i\n", c);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	return (0);
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
 }
