f2b2e4b7b3eb8451652ce4b36763e3a1dd62fed3
Maranata <maranataflk9@gmail.com>
2022-05-02 07:27:49 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
+ * main - prints program nmae, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Retrurn: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
