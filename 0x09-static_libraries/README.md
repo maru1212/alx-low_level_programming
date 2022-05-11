3cb61adc33d4d5d37fe14f61c95f2e9fd0f52951
Maranata <maranataflk9@gmail.com>
2022-05-11 09:54:22 -0700

    tasks

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This directory includes all the tasks for 0x09-static_libraries
---
+++ 0x09-static_libraries/functions/files.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+touch 3-islower.c 3-puts.c 3-strcmp.c 3-strspn.c 4-isalpha.c 4-strpbrk.c 5-strstr.c 6-abs.c 9-strcpy.c _putchar.c 
